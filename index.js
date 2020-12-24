46c5d0cfbb567952d07b9520a4fbb9a0c99412fb
KauanAmorim <kauanamorim88@gmail.com>
2020-12-23 22:53:01 -0300

    construcao de um roteamento basico

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at user')
+})
+
 app.listen(port, () => {
     console.log(`Exemple app listening at http://localhost:${port}`)
 })

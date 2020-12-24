3a467b1677371950941d9f80ec451450e6e36689
KauanAmorim <kauanamorim88@gmail.com>
2020-12-24 00:19:57 -0300

    manipulando rotas

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// rotas básicas
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
@@ -19,6 +21,37 @@ app.delete('/user', (req, res) => {
     res.send('Got a DELETE request at user')
 })
 
-app.listen(port, () => {
-    console.log(`Exemple app listening at http://localhost:${port}`)
+// manipulando rotas
+
+const func1 = function (req, res, next) {
+    console.log('func1')
+    next()
+}
+
+const func2 = function (req, res, next) {
+    console.log('func2')
+    next()
+}
+
+const func3 = function (req, res, next) {
+    console.log('func3')
+    next()
+}
+
+app.get('/manipulacoesemanipulacao', [func1, func2, func3], (req, res, next) => {
+    console.log('The response will be sent by the next function!')
+    next()
+}, (req, res) => {
+    res.send('Hello manipulador')
 })
+
+app.get('/multiplasmanupulacoes', [func1, func2, func3])
+
+app.get('/manupuladoresderota', (req, res, next) => {
+    console.log('The response will be sent by the next function!')
+    next()
+}, (req, res) => {
+    res.send('Hello manipulador de rota')
+})
+
+app.listen(port)
